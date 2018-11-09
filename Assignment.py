48cea95e9a7fafad40c2275ed12bb2b36d7f7f58
Marius <marius.diedrich@web.de>
2018-11-09 20:34:49 +0100

    Implementation of merge sort function

--- Assignment.py
+++ Assignment.py
@@ -5,7 +5,7 @@ Created on Thu Nov  8 10:27:14 2018
 
 @author: MariusD
 """
-
+#%%
 def bubble(lst):
     for j in range(0, len(lst) - 1):
         for i in range(0, len(lst) - j - 1):
@@ -13,3 +13,14 @@ def bubble(lst):
                 temp = lst[i]
                 lst[i] = lst[i + 1]
                 lst[i + 1] = temp
+  #%%
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    
+    middle = len(lst) // 2
+    left = lst[:middle]
+    right = lst[middle:]
+
+    return merge(merge_sort(left), merge_sort(right))        
+#%%
